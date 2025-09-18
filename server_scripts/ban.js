66098218da970e8307698971744ab8b4ea1da216
SelfAbandonment <1879816281@qq.com>
2025-09-18 15:16:15 +0800

    更新

--- server_scripts/ban.js
+++ server_scripts/ban.js
@@ -9,7 +9,8 @@ const restrictedItems = [
     'create:brown_toolbox',
     'create:creative_fluid_tank',
     'irons_spellbooks:arcane_anvil',
-    'automobility:rear_attachment'
+    'automobility:rear_attachment',
+    'create:hangheld_worldshaper'
 ];
 const itemChineseNames = {
     'create:creative_crate': '创造板条箱',
@@ -22,7 +23,8 @@ const itemChineseNames = {
     'create:brown_toolbox': '棕色工具箱',
     'create:creative_fluid_tank': '创造流体储罐',
     'irons_spellbooks:arcane_anvil': '奥术铁砧',
-    'automobility:rear_attachment':'车尾箱子附件'
+    'automobility:rear_attachment':'车尾箱子附件',
+    'create:hangheld_worldshaper':'世界塑形器'
 };
 function getItemChineseName(itemId) {
     return itemChineseNames[itemId] || itemId;
